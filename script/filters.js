9441cb0c7f1332bb4db33278fbcb4615a98658c9
Brahim Zerd <brahim.zerd@live.fr>
2022-09-19 19:29:27 +0200

    barre de recherche/ fonction une par une avec conditions// changement de facon de procéder// réécriture de plusieurs petites fonctions

--- script/filters.js
+++ script/filters.js
@@ -1,7 +1,3 @@
-const arrowSelectMenu = document.getElementById('select-rotate-ingredients')
 
 
-ingredientsDiv.addEventListener('click',function(){
-    arrowSelectMenu.classList.toggle('rotate')
-})
 
--- script/index.js
+++ script/index.js
@@ -11,12 +11,13 @@ const placeholder = document.getElementById('ingredients-select').getAttribute("
 const tagField = document.getElementById('tag-field');
 const arrowIngredients = document.getElementById('select-rotate-ingredients');
 const ingredientsInput = document.getElementById('ingredients-dropdown');
+const appareilsInput = document.getElementById('appareils-dropdown');
 InputDiv.parentNode.appendChild(inputError)
 
 ingredientsFilter.addEventListener('click',openFilterBar);
-ingredientsFilter.addEventListener('keyup',filterInput) 
+ingredientsFilter.addEventListener('keyup',filterIngredientsTag) 
 SearchInput.addEventListener('keyup',filterMainSearch);
-SearchInput.addEventListener('keyup',filterbyIngredients)
+
 
 
 
@@ -42,40 +43,11 @@ function openFilterBar() {
 }
 
 //fonction de filtre de la barre de recherche principal par nom & description
-function filterData() {
-    
-    const searchedString = e.target.value;
-    let filteredName =recipes.filter(element => element.name.toLowerCase().includes(searchedString));
-    let filteredDescription = recipes.filter(recipe => recipe.description.toLowerCase().includes(searchedString));
-        
-    let filteredWithDoubles = filteredName.concat(filteredDescription);
-    let filteredTotal = filteredWithDoubles.filter( (ele,pos)=>filteredWithDoubles.indexOf(ele) == pos);
 
-
-
-
-
-
-
-    if(searchedString.length < 3 && searchedString.length > 0){
-        inputError.innerHTML = "Veuillez indiquer au minimum 3 caractères dans le champs"
-        inputError.style.color = "red"
-        inputError.style.marginLeft ="1%"
-    } else {
-        
-        inputError.innerHTML = "";
-        recipesField.innerHTML = "";
-        createAllCards(filteredTotal);
-        if (filteredTotal == 0 ) {
-            recipesField.innerHTML = "Aucune recette ne correspond à votre critère… vous pouvez chercher « tarte aux pommes », « poisson », etc."
-        }
-    } 
-      
-}
 function filterByName(e) {
     let searchedString = e.target.value;
     const filterName = recipes.filter(element => element.name.toLowerCase().includes(searchedString));
-    console.log(filterName)
+    
     if(filterName.length > 0){
     console.log('name')
     createAllCards(filterName);
@@ -111,26 +83,35 @@ function filterByDescription(e){
 
 function filterbyIngredients(e){
     let searchedString = e.target.value;
+   
     recipes.map(recipe => {
-        
+        let newArrayForIngredients = [];
         recipe.ingredients.map(ingredient => {
-        const ingredientObject = {ingredient}
+        const ingredientObject = {ingredient};
+        
         if(ingredientObject.ingredient.ingredient.toLowerCase().includes(searchedString)) {
             
-            //console.log(ingredientObject.ingredient)
-               
-            return true 
-        } else {
-            
-            return false;
+            newArrayForIngredients.push(recipe)
+            return newArrayForIngredients
         } 
+        createAllCards(newArrayForIngredients)}
         
-        
-    })})}
+    )
+})
+    if(recipesField.firstChild){
+    
+    }else {
+    recipesField.innerHTML =`Aucune recette ne correspond à votre critère... vous pouvez chercher "tarte aux pommes"; "poisson", etc.`
+    }
+};
+
+
+
+
     
 
 
-
+console.log(recipes)
 
 //intégration des listes dans le filtre ingrédients 
 recipes.map(recipe => {
@@ -140,15 +121,28 @@ recipes.map(recipe => {
         ingredientsInput.appendChild(li)
         li.classList.add('float')
         li.innerHTML = ingredientObject.ingredient.ingredient
+        
     })})
-
+    let arrAppareils = [];
+    let filteredAppareils;
+    recipes.map(recipe => {
+        const appareilObject = recipe.appliance
+            arrAppareils.push(appareilObject)
+            const li = document.createElement('li'); 
+            appareilsInput.appendChild(li)
+            li.classList.add('float')
+            li.innerHTML = "";
+            filteredAppareils = arrAppareils.filter(function(ele , pos){
+                return arrAppareils.indexOf(ele) == pos;
+            })})
+           console.log( filteredAppareils )  
 
 //fonction pour filtrer les ingrédients dans la barre de recherche "Ingredients"
-function filterInput(){
-    let inputValue = target.value;
+function filterIngredientsTag(e){
+    let inputValue = e.target.value;
     ingredientsInput.innerHTML="";
-    let ok = upperCaseArrayIngredients.filter(ingredient => ingredient.toLowerCase().includes(inputValue));
-    let displayIngredients = filteredArray.filter(ingredient => ingredient.toLowerCase().includes(inputValue));
+    
+    let displayIngredients = recipes.filter(ingredients => ingredients.toLowerCase().includes(inputValue));
     displayIngredients.slice(-30).forEach(ingredient => {
         const li = document.createElement('li');
         ingredientsInput.appendChild(li)
@@ -156,15 +150,18 @@ function filterInput(){
         li.innerHTML = ingredient
     })
 }
+function filterAppareilTag(){
+    let inputValue = target.value;
+    appareilsInput.innerHTML = "";
+    let ok
+}
 
-    
-   
-   
-    
-   
-        
-        
 
+   
+   
+
+   
+ 
 
         
         
